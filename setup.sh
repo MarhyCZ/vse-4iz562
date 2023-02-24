a09775732772feda946b51d0cddd13a35330003f
Michal Marhan <marhanmichal@gmail.com>
2023-02-24 07:35:09 +0100

    Always restart

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 echo "Creating Docker Network named Traefik"
 docker network create traefik
-docker-compose -f server/docker-compose.yml up -d
+docker-compose -f traefik/docker-compose.yml up -d
 docker-compose -f docker-compose.yml up -d
